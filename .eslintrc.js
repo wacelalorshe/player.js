50a3bc58802f89cbca4c01b607d52a5f75b6d3c2
Wesley Luyten <me@wesleyluyten.com>
2020-08-26 19:10:15 -0400

    Upgrade eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
-'use strict';
-
-const OFF = 0;
-const WARNING = 1;
+// const OFF = 0;
+// const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
     extends: '@vimeo/eslint-config-player/es6',
+    parserOptions: {
+        ecmaVersion: 8,
+        sourceType: 'module',
+        allowImportExportEverywhere: true
+    },
+    env: {
+        browser: true,
+        node: true
+    },
     plugins: ['compat'],
+    globals: {
+        XDomainRequest: false
+    },
     rules: {
         'compat/compat': ERROR
     },
     settings: {
-        'polyfills': [
+        polyfills: [
             'Promise'
         ]
     }
