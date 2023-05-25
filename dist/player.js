89f6e771059d2748b65ae045dc7b1fdf0c080800
Rowan Krishnan <rowan.krishnan@vimeo.com>
2023-05-25 15:23:38 -0400

    v2.20.1

--- dist/player.js
+++ dist/player.js
@@ -1,4 +1,4 @@
-/*! @vimeo/player v2.20.0 | (c) 2023 Vimeo | MIT License | https://github.com/vimeo/player.js */
+/*! @vimeo/player v2.20.1 | (c) 2023 Vimeo | MIT License | https://github.com/vimeo/player.js */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
