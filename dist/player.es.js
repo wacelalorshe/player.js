ef0b86bac0cf96f4d93f291ddd62bb8532c1c999
Artur Parkhisenko <arturparkhisenko@users.noreply.github.com>
2024-08-20 13:22:54 -0400

    v2.24.0

--- dist/player.es.js
+++ dist/player.es.js
@@ -1,4 +1,4 @@
-/*! @vimeo/player v2.23.1 | (c) 2024 Vimeo | MIT License | https://github.com/vimeo/player.js */
+/*! @vimeo/player v2.24.0 | (c) 2024 Vimeo | MIT License | https://github.com/vimeo/player.js */
 function ownKeys(object, enumerableOnly) {
   var keys = Object.keys(object);
   if (Object.getOwnPropertySymbols) {
